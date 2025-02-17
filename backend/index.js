a6b22e05a4bbd162c8e1ad739648480f8e9ef453
Dattu Gollapinni <101050977+dattu20038@users.noreply.github.com>
2025-02-17 10:15:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ const { authenticateToken } = require("./utilities");
 
 const User = require("./models/user.model");
 const TravelStory = require("./models/travelStory.model");
+const MONGO_URI = "mongodb+srv://karthikeyareddy2nd:nBJld47MwupXvCMM@cluster0.i4k8j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
